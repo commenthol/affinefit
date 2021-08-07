6a604912d6fec07f1318238df58e19f7fd51f819
commenthol <commenthol@gmail.com>
2021-08-07 12:46:49 +0200

    chore: bump dependencies
    fix linter issues

--- test/affineFit.mocha.js
+++ test/affineFit.mocha.js
@@ -2,18 +2,18 @@
 
 'use strict'
 
-var assert = require('assert')
-var affineFit = require('..')
+const assert = require('assert')
+const affineFit = require('..')
 
 describe('#affineFit', function () {
   it('can transform a rectangle', function () {
-    var fromPts = [[1, 1], [1, 2], [2, 2], [2, 1]] // a 1x1 rectangle
-    var toPts = [[4, 4], [6, 6], [8, 4], [6, 2]]   // scaled x 2, rotated 45 degrees and translated
+    const fromPts = [[1, 1], [1, 2], [2, 2], [2, 1]] // a 1x1 rectangle
+    const toPts = [[4, 4], [6, 6], [8, 4], [6, 2]]   // scaled x 2, rotated 45 degrees and translated
 
-    var trn = affineFit(fromPts, toPts)
+    const trn = affineFit(fromPts, toPts)
 
     fromPts.forEach((pt, idx) => {
-      var res = trn(pt).map((f) => {
+      const res = trn(pt).map((f) => {
         return parseFloat(f.toPrecision(10), 10)
       })
       // console.log(pt, '==>', res)
--- test/example.js
+++ test/example.js
@@ -2,20 +2,20 @@
 
 'use strict'
 
-var fs = require('fs')
-var path = require('path')
-var affineFit = require('..')
+const fs = require('fs')
+const path = require('path')
+const affineFit = require('..')
 
-var svgIn = fs.readFileSync(path.join(__dirname, 'rectangles.svg'), 'utf8')
+const svgIn = fs.readFileSync(path.join(__dirname, 'rectangles.svg'), 'utf8')
 
-var fromPts = [[1, 1], [1, 2], [2, 2], [2, 1]]  // a 1x1 rectangle
-var toPts   = [[4, 4], [6, 6], [8, 4], [6, 2]]  // scaled x 2, rotated 45 degrees and translated
-var trn = affineFit(fromPts, toPts) // affine transformation
+const fromPts = [[1, 1], [1, 2], [2, 2], [2, 1]]  // a 1x1 rectangle
+const toPts   = [[4, 4], [6, 6], [8, 4], [6, 2]]  // scaled x 2, rotated 45 degrees and translated
+const trn = affineFit(fromPts, toPts) // affine transformation
 
 function transform (svg) {
   svg = svg.replace(/(<path d=")([^"]*)(".*?>)/g, function (m, m1, m2, m3) {
     m2 = m2.replace(/([ML]) (\d+) (\d+)/g, function (m, m1, m2, m3) {
-      var res = trn([parseInt(m2, 10), parseInt(m3, 10)]).map((f) => {
+      const res = trn([parseInt(m2, 10), parseInt(m3, 10)]).map((f) => {
         return Math.round(f)
       })
       return [m1, res[0], res[1] + 800].join(' ')
@@ -25,6 +25,6 @@ function transform (svg) {
   return svg
 }
 
-var svgOut = transform(svgIn)
+const svgOut = transform(svgIn)
 
 fs.writeFileSync(path.join(__dirname, 'transformed.svg'), svgOut, 'utf8')
